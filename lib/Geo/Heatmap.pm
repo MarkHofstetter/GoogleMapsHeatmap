5d8b01b758a4231687fdbc77f196ca7e8e68ea73
Mark Hofstetter <mark@univie.ac.at>
2013-12-05 15:49:57 +0100

    imager

--- lib/Geo/Heatmap.pm
+++ lib/Geo/Heatmap.pm
@@ -1,7 +1,6 @@
 package Geo::Heatmap;
 use Moose;
 use Geo::Heatmap::USNaviguide_Google_Tiles;
-use Image::Magick;
 use Imager;
 use Storable;
 
@@ -11,9 +10,8 @@ has 'logfile'       => (isa => 'Str', is => 'rw');
 has 'return_points' => (isa => 'CodeRef', is => 'rw'); 
 has 'zoom_scale'    => (isa => 'HashRef', is => 'rw'); 
 has 'palette'       => (isa => 'Str', is => 'rw');
+has 'scale'         => (isa => 'Int', is => 'rw', default => 1);
 has 'blur'          => (isa => 'Int', is => 'rw', default => 4);  
-
-__PACKAGE__->meta->make_immutable;
 
 our $VERSION = '0.15';
 
@@ -91,7 +89,7 @@ sub calc_hm_tile {
   my $maxval = ($bin)**2;
   # $zoom_scale->{$z} |= 0;
   my $defscale = $zoom_scale->{$z} > 0 ? $zoom_scale->{$z} : $maxval;
-  $defscale *= 1.1;
+  $defscale *= 1.1 ;
   my $scale = 500/log($defscale);
   my $max = 256/$bin - 1;
   my $dmax = 0;
@@ -119,7 +117,7 @@ sub calc_hm_tile {
   }
   
   if ($self->logfile) {
-    open (LOG, ">>");
+    open (LOG, ">>". $self->logfile);
     printf LOG  "densitylog:  x y z pointcount: %s %s %s %s\n", $x, $y, $z, $dmax;
     close LOG;
   }
@@ -128,32 +126,6 @@ sub calc_hm_tile {
   $self->cache->set($mca, $ubblob);
   return $ubblob;
 }
-
-
-sub create_tile {
-  my ($tile) = @_;
-  my ($x, $y, $z) = split(/\s+/, $tile);
-  my $value      = &Google_Tile_Factors($z, 0) ;
-  my %r = Google_Tile_Calc($value, $y, $x);
-
-  my $image = Image::Magick->new(magick=>'png');
-  $image->Set(size=>'256x256');
-  $image->ReadImage('xc:white');
-  my $ps = get_points(\%r);
-
-  foreach my $p (@$ps) {
-    my @d = Google_Coord_to_Pix($value, $p->[0], $p->[1]);
-    my $ix = $d[1] - $r{PXW};
-    my $iy = $d[0] - $r{PYN};
-    $image->Set("pixel[$ix,$iy]"=>"rgb(0, 0, 89)");
-  }
-
-  my $nw = sprintf("%s %s", $r{LATN}, $r{LNGW});
-  my $se = sprintf("%s %s", $r{LATS}, $r{LNGE});
-  my ($blob) =  $image->ImageToBlob();
-  return $blob;
-}
-
 
 1;
 
@@ -164,18 +136,20 @@ __END__
 
 =head1 NAME
 
-Geo::Heatmap - generate a density map (aka heatmap) overlay layer for Google Maps
+Geo::Heatmap - generate a density map (aka heatmap) overlay layer for Google Maps, see the www directory in the distro how it works
 
 =head1 VERSION
 
-version 0.07
+version 0.15
 
 =head1 REQUIRES
 
-L<Moose>
-L<Storable>
-L<CHI>
-L<Image::Magick>
+=begin html
+
+Moose <br>
+Storable <br>
+CHI <br>
+Imager <br>
 
 =head1 METHODS
 
@@ -197,8 +171,100 @@ palette
 
 =head2 USAGE
 
-    Create a Heatmap layer for GoogleMaps
+Create a Heatmap layer for GoogleMaps
 
+=head3 The HTML part
+
+=begin html
+<pre>
+<code>
+  &lt;head&gt;
+     &lt;meta name="viewport" content="initial-scale=1.0, user-scalable=no" /&gt;
+     &lt;style type="text/css"&gt;
+       html { height: 100% }
+       body { height: 100%; margin: 0; padding: 0 }
+       #map-canvas { height: 100% }
+     &lt;/style&gt;
+     &lt;script type="text/javascript"
+       src="https://maps.googleapis.com/maps/api/js?key=<yourapikey>&sensor=true"&gt;
+     &lt;/script&gt;
+     &lt;script type="text/javascript"&gt;
+       var overlayMaps = [{
+         getTileUrl: function(coord, zoom) {
+           return "hm.fcgi?tile="+coord.x+"+"+coord.y+"+"+zoom;
+         },
+ 
+         tileSize: new google.maps.Size(256, 256),
+         isPng: true,
+         opacity: 0.4
+       }];
+ 
+       function initialize() {
+         var mapOptions = {
+           center: new google.maps.LatLng(48.2130, 16.375),
+           zoom: 9
+         };
+         var map = new google.maps.Map(document.getElementById("map-canvas"),
+             mapOptions);
+ 
+       var overlayMap = new google.maps.ImageMapType(overlayMaps[0]);
+       map.overlayMapTypes.setAt(0,overlayMap);
+ 
+       }
+       google.maps.event.addDomListener(window, 'load', initialize);
+ 
+     &lt;/script&gt;
+   &lt;/head&gt;
+   &lt;body&gt;
+     &lt;div id="map-canvas"/&gt;
+  &lt;/body&gt;
+</code>
+</pre>
+<br>
+
+=end html
+
+=head3 The (f)cgi part
+
+<pre>
+<code>
+  #!/usr/bin/env perl
+  
+  use strict;
+  use FCGI;
+  use DBI;
+  use CHI;
+  use FindBin qw/$Bin/;
+  use lib "$Bin/../lib";
+  
+  use Geo::Heatmap;
+  
+  #my $cache = CHI->new( driver  => 'Memcached::libmemcached',
+  #    servers    => [ "127.0.0.1:11211" ],
+  #    namespace  => 'GoogleMapsHeatmap',
+  #);
+  
+  
+  my $cache = CHI->new( driver => 'File',
+           root_dir => '/tmp/GoogleMapsHeatmap'
+       );
+  
+  
+  our $dbh = DBI->connect("dbi:Pg:dbname=gisdb", 'gisdb', 'gisdb', {AutoCommit => 0});
+  
+  my $request = FCGI::Request();
+  
+  while ($request->Accept() >= 0) {
+    my $env = $request->GetEnvironment();
+    my $p = $env->{'QUERY_STRING'};
+  
+    my ($tile) = ($p =~ /tile=(.+)/);
+    $tile =~ s/\+/ /g;
+  
+    # package needs a CHI Object for caching
+    #               a Function Reference to get LatLOng within a Google Tile
+    #               maximum number of points per zoom level
+  
     my $ghm = Geo::Heatmap->new();
     $ghm->palette('palette.store');
     $ghm->zoom_scale( {
@@ -221,38 +287,88 @@ palette
       17 => 2,
       18 => 0,
     } );
+  
+  sub get_points {
+    my $r = shift;
+  
+    my $sth = $dbh->prepare( qq(select ST_AsEWKT(geom) from geodata
+                           where geom &&
+                ST_SetSRID(ST_MakeBox2D(ST_Point($r->{LATN}, $r->{LNGW}),
+                                        ST_Point($r->{LATS}, $r->{LNGE})
+                          ),4326))
+                );
+  
+    $sth->execute();
+  
+    my @p;
+    while (my @r = $sth->fetchrow) {
+      my ($x, $y) = ($r[0] =~/POINT\((.+?) (.+?)\)/);
+      push (@p, [$x ,$y]);
+    }
+    $sth->finish;
+    return \@p;
+  }
+  </code>
+  </pre>
 
-    $ghm->cache($cache);
-    $ghm->return_points( \&get_points );
-    my $image = $ghm->tile($tile);
+=end html 
 
 
-    You need a color palette (one is included) to encode values to colors, in Storable Format as an arrayref of arrayrefs eg
-      [50] = [34, 45, 56]
-    which means that a normalized value of 50 would lead to an RGB color of 34% red , 45% blue, 56% green
+You need a color palette (one is included) to encode values to colors, in Storable Format as 
+an arrayref of arrayrefs eg
 
-    zoom_scale
-      the maximum number of points for a given google zoom scale, you would be able to extract to values from the denisity log
-      or derive them from your data in some cunning way
+    [50] = [34, 45, 56]
 
-    cache
-      you need some caching for the tiles otherwise the map would be quite slow. Use a CHI object with the cache you like
+which means that a normalized value of 50 would lead to an RGB color of 34% red , 45% blue, 
+56% green.
 
-    return_points
-      is a function reference which expects a single hashref as a parameter which defines two LAT/LONG points to get all
-      data points within this box
+=over 4
+
+=item zoom_scale
+
+The maximum number of points for a given google zoom scale. You would be able to extract 
+to values from the denisity log or derive them from your data in some cunning way
+
+=item cache
+
+You need some caching for the tiles otherwise the map would be quite slow. Use a CHI object 
+with the cache you like
+
+=item return_points
+
+A function reference which expects a single hashref as a parameter which defines two LAT/LONG 
+points to get all data points within this box:
+
       $r->{LATN}, $r->{LNGW}), $r->{LATS}, $r->{LNGE}
-      the function has to return an arrayref of arrayrefs of the points within the box
 
-    tile
-      returns the rendered image
+The function has to return an arrayref of arrayrefs of the points within the box
 
+=item tile
 
+Returns the rendered image
 
+=back
 
 =head1 AUTHOR
 
 Mark Hofstetter <hofstettm@cpan.org>
+
+  Thanks to 
+  brian d foy
+  Marcel Gruenauer
+  David Steinbrunner
+
+=head1 TODO
+
+=begin html
+
+<ul>
+  <li> put more magic in calculation of zoom scales </li>
+  <li> make more things configurable </li>
+  <li> add even more tests </li>
+</ul>
+
+=end html
 
 =head1 COPYRIGHT AND LICENSE
 
